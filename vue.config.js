efafb391e9986e3057ffd318bce66110e10895c5
Nicolas Boeuf <nicolas_boeuf@hotmail.fr>
2025-01-20 14:53:44 +0100

    initialize vue app

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack')
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1
+      })
+    ]
+  },
+  filenameHashing: false,
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === "production" ? "/offre-inclusion/" : "/",
+  chainWebpack:
+    config => {
+      config.optimization.delete('splitChunks')
+    }
+}
