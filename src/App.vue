152b3da280e8e19cb4b05548ca188204d6837172
Nicolas Boeuf <nicolas_boeuf@hotmail.fr>
2025-02-04 18:47:47 +0100

    build tabs

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ export default {
   },
   data(){
     return {
-      "page":"zoom"
+      "page":"ensemble"
     }
   },
   computed: {
--- src/components/vueTableau.vue
+++ src/components/vueTableau.vue
@@ -1,21 +1,85 @@
 <template>
   <div id="vueTerritoire">
+    
+    <div class="filters_selector">
+        <h4>Filtrez les données</h4>
+        <div class="filters_selector_item">
+            <div class="filters_box" ref="communeDropdown">
+                <div @click="toggleDropdown">
+                {{ selectedCommune ? formatCommuneName(population.find(c => c.insee === selectedCommune).nom_commune) : 'Toutes les communes' }}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                    <path d="M7.99999 9.99997L5.17133 7.1713L6.11466 6.22864L7.99999 8.11464L9.88533 6.22864L10.8287 7.1713L7.99999 9.99997Z" fill="black"/>
+                </svg>
+                </div>
+                <div class="dropdown-content" v-if="isDropdownOpen">
+                <div 
+                    class="dropdown-item"
+                    @click="selectCommune(null)"
+                >
+                    Toutes les communes
+                </div>
+                <div 
+                    class="dropdown-separator"
+                ></div>
+                <div 
+                    v-for="commune in population" 
+                    :key="commune.insee"
+                    class="dropdown-item"
+                    @click="selectCommune(commune)"
+                >
+                    {{ formatCommuneName(commune.nom_commune) }}
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="filters_selector_item">
+            <div class="filters_box" ref="thematiqueDropdown">
+                <div @click="toggleThematiqueDropdown">
+                    {{ selectedThematique ? formatThemeName(selectedThematique) : 'Toutes les thématiques' }}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                        <path d="M7.99999 9.99997L5.17133 7.1713L6.11466 6.22864L7.99999 8.11464L9.88533 6.22864L10.8287 7.1713L7.99999 9.99997Z" fill="black"/>
+                    </svg>
+                </div>
+                <div class="dropdown-content" v-if="isThematiqueDropdownOpen">
+                    <div 
+                        class="dropdown-item"
+                        @click="selectThematique(null)"
+                    >
+                        Toutes les thématiques
+                    </div>
+                    <div class="dropdown-separator"></div>
+                    <div 
+                        v-for="thematique in thematiques" 
+                        :key="thematique"
+                        class="dropdown-item"
+                        @click="selectThematique(thematique)"
+                    >
+                        {{ formatThemeName(thematique) }}
+                    </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    
     <h2>Chiffres clés</h2>
-    <div v-if="structuresData" class="chiffre_box_container">
+    
+    <div v-if="structuresData&&servicesData" class="chiffre_box_container">
         <div class="chiffre_box">
-            <span class="chiffre">{{structuresData.length.toLocaleString()}}</span>
+            <span class="chiffre">{{filteredStructures.length.toLocaleString()}}</span>
             <span class="text">structures</span>
         </div>
         <div class="chiffre_box">
-            <span class="chiffre">{{servicesData.length.toLocaleString()}}</span>
+            <span class="chiffre">{{filteredServices.length.toLocaleString()}}</span>
             <span class="text">services</span>
         </div>
         <div class="chiffre_box">
-            <span class="chiffre">{{(servicesData.length/structuresData.length).toFixed(1).toLocaleString()}}</span>
+            <span class="chiffre">{{(filteredServices.length/filteredStructures.length).toFixed(1).toLocaleString()}}</span>
             <span class="text">services<br>par structure</span>
         </div>
         <div class="chiffre_box">
-            <span class="chiffre">98</span>
+            <span class="chiffre">{{ populationRatio.toFixed(1).toLocaleString() }}</span>
             <span class="text">services pour<br>10 000 habitants</span>
         </div>
     </div>
@@ -102,6 +166,7 @@
 
 <script>
 import store from '@/store'
+import population from '../../public/data/population.json'
 
 export default {
   name: 'VueTableau',
@@ -114,6 +179,11 @@ export default {
       structuresCurrentPage: 1,
       itemsPerPage: 5,
       thematiques: ["famille","numerique","remobilisation","accompagnement-social-et-professionnel-personnalise","sante","acces-aux-droits-et-citoyennete","handicap","se-former","mobilite","preparer-sa-candidature","logement-hebergement","creation-activite","trouver-un-emploi","gestion-financiere","choisir-un-metier","equipement-et-alimentation","illettrisme","souvrir-a-linternational","apprendre-francais"],
+      selectedThematique: null,
+      selectedCommune: null,
+      isDropdownOpen: false,
+      isThematiqueDropdownOpen: false,
+      population,
     }
   },
   computed: {
@@ -123,27 +193,87 @@ export default {
     structuresData() {
       return store.state.structuresData
     },
+    filteredServices() {
+      var communesFiltredServices
+      if (!this.selectedCommune) {
+        communesFiltredServices = this.servicesData;
+      }else{
+        communesFiltredServices = this.servicesData.filter(service => service['Code Insee'] === this.selectedCommune);
+      }
+      if (this.selectedThematique) {
+        communesFiltredServices = communesFiltredServices.filter(service => {
+          const thematiques = Array.isArray(service.Thematiques) ? service.Thematiques : [service.Thematiques];
+          return thematiques.some(t => t.includes(this.selectedThematique));
+        });
+      }
+      return communesFiltredServices;
+    },
+    filteredStructures() {
+      var communesFiltredStructures
+      if (!this.selectedCommune) {
+        communesFiltredStructures = this.structuresData;
+      }else{
+        communesFiltredStructures = this.structuresData.filter(structure => structure['Code Insee'] === this.selectedCommune);
+      }
+      if(this.selectedThematique){
+        communesFiltredStructures = communesFiltredStructures.filter(structure => {
+          return this.filteredServices.some(service => service['Structure ID'] === structure.ID);
+        });
+      }
+      return communesFiltredStructures;
+    },
     paginatedServices() {
       const start = (this.currentPage - 1) * this.itemsPerPage;
       const end = start + this.itemsPerPage;
-      return this.servicesData.slice(start, end);
+      return this.filteredServices.slice(start, end);
     },
     totalPages() {
-      return Math.ceil(this.servicesData.length / this.itemsPerPage);
+      return Math.ceil(this.filteredServices.length / this.itemsPerPage);
     },
     paginatedStructures() {
       const start = (this.structuresCurrentPage - 1) * this.itemsPerPage;
       const end = start + this.itemsPerPage;
-      return this.structuresData.slice(start, end);
+      return this.filteredStructures.slice(start, end);
     },
     structuresTotalPages() {
-      return Math.ceil(this.structuresData.length / this.itemsPerPage);
+      return Math.ceil(this.filteredStructures.length / this.itemsPerPage);
+    },
+    populationRatio(){
+        var totalPopulation
+        if (!this.selectedCommune) {
+            totalPopulation = population.reduce((sum, commune) => sum + parseInt(commune.population), 0);
+            return (this.filteredServices.length / totalPopulation) * 10000;
+        }else{
+            totalPopulation = population.find(commune => commune.insee === this.selectedCommune).population;
+            return (this.filteredServices.length / totalPopulation) * 10000;
+        }
     }
   },
   created(){
     
   },
   methods: {
+    toggleDropdown() {
+      this.isDropdownOpen = !this.isDropdownOpen;
+    },
+    selectCommune(commune) {
+      this.selectedCommune = commune.insee;
+      this.isDropdownOpen = false;
+    },
+    handleClickOutside(event) {
+      if (this.$refs.communeDropdown && !this.$refs.communeDropdown.contains(event.target)) {
+        this.isDropdownOpen = false;
+      }
+      if (this.$refs.thematiqueDropdown && !this.$refs.thematiqueDropdown.contains(event.target)) {
+        this.isThematiqueDropdownOpen = false;
+      }
+    },
+    formatCommuneName(name) {
+        return name.toLowerCase()
+          .split(/(?=[- ])|(?<=[- ])/)
+          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+          .join('');
+    },
     formatThematiques(thematiquesString) {
       if (!thematiquesString) return '';
       const matchingThemes = this.thematiques
@@ -178,12 +308,19 @@ export default {
       return accentsMap[theme] || theme;
     },
     countServicesForStructure(structureId) {
-      return this.servicesData.filter(service => service['Structure ID'] === structureId).length;
+      return this.filteredServices.filter(service => service['Structure ID'] === structureId).length;
     },
     getStructureName(structureId) {
       const structure = this.structuresData.find(s => s.ID === structureId);
       return structure ? structure.Nom : structureId;
-    }
+    },
+    toggleThematiqueDropdown() {
+      this.isThematiqueDropdownOpen = !this.isThematiqueDropdownOpen;
+    },
+    selectThematique(thematique) {
+      this.selectedThematique = thematique;
+      this.isThematiqueDropdownOpen = false;
+    },
   }
 }
 </script>
@@ -191,6 +328,9 @@ export default {
 <style lang="scss">
 @import "../../css/overload-fonts.css";
 
+.datavue{
+    margin-bottom: 100px;
+}
 
 .chiffre_box_container {
   display: flex;
@@ -292,6 +432,11 @@ export default {
   tr {
     font-weight: 400;
   }
+}
+
+.filters_selector_item{
+    display: inline-block;
+    margin-right: 1rem;
 }
 
 </style>
