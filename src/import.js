2f7d6205efe8d96c50f12b970230530b321e14c6
Nicolas Boeuf <nicolas_boeuf@hotmail.fr>
2025-02-18 16:25:09 +0100

    add api request

--- src/import.js
+++ src/import.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export const getData = async function (store) {
 
 	const dataRequest = await fetch("data/services.json")
@@ -8,6 +10,17 @@ export const getData = async function (store) {
 
 	store.commit('initializeData',data)
 	store.commit('initializeStructuresData',structures)
+	
+	var API_KEY = process.env.VUE_APP_MB_API_KEY
+	
+	const options = {method: 'GET', headers: {'Content-Type': 'application/json', 'X-API-KEY': API_KEY}, url: 'https://stats.inclusion.beta.gouv.fr/api/card/5707'};
+
+	try {
+		const { data } = await axios.request(options);
+		console.log(data);
+	} catch (error) {
+		console.error(error);
+	}
 	
 	store.commit("endImport",true)
 
